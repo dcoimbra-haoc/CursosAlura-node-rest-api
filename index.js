fb747c07e5ea1e5579e1e58fcb5419c74e1636e6
dcoimbra-haoc <74983419+dcoimbra-haoc@users.noreply.github.com>
2021-06-17 10:18:43 -0300

    primeiro curso

---
+++ index.js
@@ -0,0 +1,16 @@
+const customExpress = require('./config/customExpress')
+const conexao = require('./infra/conex')
+const Tabelas = require('./infra/tabelas')
+
+conexao.connect((erro) => {
+    if (erro) {
+        console.log(erro)
+    } else {
+        console.log('Conectado com sucesso')
+
+        Tabelas.init(conexao)
+        const app = customExpress()
+
+        app.listen(3000, () => console.log('servidor rodando na porta 3000'))
+    }
+})
